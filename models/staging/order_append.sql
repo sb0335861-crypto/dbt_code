841e00b5e4366cc43e4e5c74f4207a65758fa063
sb0335861@gmail.com <sb0335861@gmail.com>
2025-12-08 01:45:37 +0000

    commit

--- models/staging/order_append.sql
+++ models/staging/order_append.sql
@@ -3,4 +3,4 @@
     incremental_strategy="append",
 ) }}
 
-select * from {{source('datafeed_shared_schema','stg_order_data')}} where id in (1,5,7)
+select * from {{source('datafeed_shared_schema','stg_order_data')}} where id in (1,5,7,8,10)
